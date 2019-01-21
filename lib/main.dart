9063dba26d7640c40a05ebfdeb02041dc8c497e8
Luis Carino <luiscarinodev@gmail.com>
2019-01-20 19:51:23 -0500

    Remove action from app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:bully_vets_app/detail_screen.dart';
-import 'package:bully_vets_app/util.dart';
 import 'package:bully_vets_app/vet.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -36,12 +35,6 @@ class _VetListWidgetState extends State<VetListWidget> {
     return Scaffold(
         appBar: AppBar(
           title: Text(widget.title),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.add),
-              onPressed: populateDatabase,
-            )
-          ],
         ),
         body: _buildBody(context));
   }
